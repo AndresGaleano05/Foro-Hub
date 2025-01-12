83069bb3edf4a899eb0ceffba6ee82a1ced0a6fa
Andres Galeano <andressmmoreno15@gmail.com>
2025-01-12 09:02:44 -0500

    Primer commit: creacion del proyecto

---
+++ src/test/java/Foro_hub/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Foro_hub.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
